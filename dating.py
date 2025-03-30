e311dd0ac21477b9a8dd6234239e936bbd3b6461
JosephWondimagnehu <joseph.wondimagnehu@stu.ocsb.ca.>
2025-03-30 13:28:22 -0400

    Fixed style errors in dating.py

--- dating.py
+++ dating.py
@@ -3,15 +3,16 @@
 # Date: Mar. 30, 2025
 """
 This program checks if a user's age is older than 25
-and younger than 40 to get an approval 
+and younger than 40 to get an approval
 from grandparents to date their grandchild. Adds additional
 checks for validating age inputs.
 """
 
+
 # Define the main function.
 def main():
     # Get the user's age as a string.
-    age_as_string = input("You want to date my grandchild? How old are you?\n")
+    age_as_string = input("You want to date our grandchild? How old are you?\n")
 
     # Try to check the validity of the entered age.
     try:
@@ -29,8 +30,10 @@ def main():
         # Otherwise, they are too young or too old for expectations.
         else:
             # Display the user is not approved of dating the grandchild.
-            print("\nYou cannot date our grandchild! "
-            "You must be older than 25 and younger than 40.")
+            print(
+                "\nYou cannot date our grandchild! "
+                "You must be older than 25 and younger than 40."
+            )
     # Runs if int() could not convert the user's input value to an integer.
     except ValueError:
         # Display to the user that their age is not an integer.
@@ -39,6 +42,7 @@ def main():
     finally:
         # Display a message thanking the user for playing.
         print("Thanks for playing!")
+
 
 # Check if the special name of the file is __main__.
 if __name__ == "__main__":
